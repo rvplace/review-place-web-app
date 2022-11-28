4b4370a5b34271e5b4dedd8764c99b5c339715bb
huyvq74 <vqhuybkhn@gmail.com>
2022-11-28 09:52:06 +0700

    update

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,63 +1,40 @@
 const Header = () => {
     return (
-        <header className="p-3 bg-dark text-white">
-            <div className="container">
-                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <a
-                        href="#"
-                        className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
-                    >
-                        LOGO
-                    </a>
-
-                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li>
-                            <a href="#" className="nav-link px-2 text-secondary">
-                                Home
-                            </a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container px-4 px-lg-5">
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation"><span
+                    className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a>
                         </li>
-                        <li>
-                            <a href="#" className="nav-link px-2 text-white">
-                                Features
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="nav-link px-2 text-white">
-                                Pricing
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="nav-link px-2 text-white">
-                                FAQs
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="nav-link px-2 text-white">
-                                About
-                            </a>
+                        <li className="nav-item"><a className="nav-link" href="#">About</a></li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
+                               data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a className="dropdown-item" href="#">All Products</a></li>
+                                <li>
+                                    <hr className="dropdown-divider"/>
+                                </li>
+                                <li><a className="dropdown-item" href="#">Popular Items</a></li>
+                                <li><a className="dropdown-item" href="#">New Arrivals</a></li>
+                            </ul>
                         </li>
                     </ul>
-
-                    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                        <input
-                            type="search"
-                            className="form-control form-control-dark"
-                            placeholder="Search..."
-                            aria-label="Search"
-                        />
+                    <form className="d-flex">
+                        <button className="btn btn-outline-dark" type="submit">
+                            <i className="bi-cart-fill me-1"></i>
+                            Cart
+                            <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
                     </form>
-
-                    <div className="text-end">
-                        <button type="button" className="btn btn-outline-light me-2">
-                            Login
-                        </button>
-                        <button type="button" className="btn btn-warning">
-                            Sign-up
-                        </button>
-                    </div>
                 </div>
             </div>
-        </header>
+        </nav>
     );
 };
 
